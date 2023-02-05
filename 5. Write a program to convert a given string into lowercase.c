8c1f27736ac6840e272d996e15d476982768995e
MD AAMIR <111450941+aamir-gitch786@users.noreply.github.com>
2023-02-05 18:05:18 +0530

    Update 5. Write a program to convert a given string into lowercase.c

--- 5. Write a program to convert a given string into lowercase.c
+++ 5. Write a program to convert a given string into lowercase.c
@@ -1,6 +1,6 @@
 /******************************************************************************
 
-4. Write a program to convert a given string into lowercase
+5. Write a program to convert a given string into lowercase
 
 *******************************************************************************/
 
